3c491696028f040043740fd314e5cc55db3a90a2
Adithya Nair <adithyadoha123@gmail.com>
2024-07-17 23:53:31 +0530

    created more tables and i think it works now

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-import psycopg2 #helps us to interact with the db =
+import psycopg2 #helps us to interact with the db 
 
 # Database connection setup
 conn = psycopg2.connect(
@@ -21,10 +21,43 @@ def users_table():
     cursor.execute(create_table_query)
     conn.commit()
 
+
+def questions_table():
+    create_table_query ='''
+    CREATE TABLE IF NOT EXISTS questions (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50),
+    question_text TEXT,
+    correct_answer TEXT,
+    answer TEXT,
+    FOREIGN KEY (username) REFERENCES users(username)
+    );
+    ''' 
+    cursor.execute(create_table_query)
+    conn.commit()
+
+def leaderboard_table():
+    create_table_query='''
+    CREATE TABLE IF NOT EXISTS leaderboard (
+    username VARCHAR(50) PRIMARY KEY,
+    points INTEGER DEFAULT 0,
+    FOREIGN KEY (username) REFERENCES users(username)
+    );
+    '''
+    cursor.execute(create_table_query)
+    conn.commit()
+    
+
+
 # Run the table creation functions
 if __name__ == "__main__":
     users_table()
     print("Users table created successfully")
+    questions_table()
+    print("Questions table created succesfully")
+    leaderboard_table()
+    print("Leaderbord table created successfully")
+
 
 # Close the database connection
 cursor.close()
